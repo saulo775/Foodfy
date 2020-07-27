05af83d8e8c790b3e7b60f154b72448bc802adce
saulo775 <59702227+saulo775@users.noreply.github.com>
2020-07-27 11:48:06 -0300

    commit 5 - servidor próprio com dependencia express

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'))
+
+server.set("view engine", "html");
+
+nunjucks.configure("views", {
+    express: server
+})
+
+
+
+server.get("/", function (req, res) {
+    return res.render("home")
+})
+
+server.get("/recipes", function (req, res) {
+    return res.render("recipes")
+})
+server.get("/about", function (req, res) {
+    return res.render("about")
+})
+
+
+
+server.listen(5000, function () {
+    console.log("Server is running!!!");
+});
