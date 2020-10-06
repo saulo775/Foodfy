f0c4becd21c455d18593f31e84ae1f2fbc7b1301
saulo775 <59702227+saulo775@users.noreply.github.com>
2020-10-05 21:15:18 -0300

    commit 9 - botão de mostrar ou esconder itens na página de detalhes da receita.

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,11 +1,29 @@
 //const indexOf = require("../../data");
 
 const recipes = document.querySelectorAll(".recipe")
+const button_hide = document.querySelectorAll(".button_hide")
+const content = document.querySelectorAll(".content")
+
+
 
 for (let recipe of recipes){
 
     recipe.addEventListener("click", function(){
         const recipeId = recipe.getAttribute("id")
         window.location.href = `http://localhost:5000/recipes/${recipeId}`
+    })
+}
+
+
+for (const button in button_hide) {
+    button_hide[button].addEventListener("click", function() {
+        if(content[button].classList.contains('hidden')) {
+            content[button].classList.remove('hidden')
+            button_hide[button].textContent = 'ESCONDER'
+        } else {
+            content[button].classList.add('hidden')
+
+            button_hide[button].textContent = 'MOSTRAR'
+        }
     })
 }
